116f098b396020199594210f8b3407fe981d7d56
markson <markson@2333ma.top>
2022-05-12 22:08:45 +0800

    v2rayA: Add version 1.5.7

--- Formula/v2raya.rb
+++ Formula/v2raya.rb
@@ -2,14 +2,14 @@ class V2raya < Formula
     desc "Web-based GUI client of Project V"
     homepage "https://v2raya.org"
     license "AGPL-3.0-only"
-    version "1.5.7-13"
+    version "1.5.7"
     ## Install v2rayA
-    $url_linux_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/1.5.7-13/v2raya-x86_64-linux.zip"
-    $sha_linux_x64 = "D3428F866B33702E2BF5339757B9168BCCB7ACB13F1D08521F7080F152488D5B"
-    $url_macos_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/1.5.7-13/v2raya-x86_64-macos.zip"
-    $sha_macos_x64 = "A212BD13A92A2536A153F1D30A55497844FE9ED2E890FB921F811E99BC628BAC"
-    $url_macos_arm64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/1.5.7-13/v2raya-aarch64-macos.zip"
-    $sha_macos_arm64 = "CBA6579BDA84F81FAAA961E423F3265C90D71013DED017A2E98BFDF562517AB2"
+    $url_linux_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/1.5.7/v2raya-x86_64-linux.zip"
+    $sha_linux_x64 = "A05EB77538B989841B53421B9B8F5B5607D899A55B95DDC5E1F7942CC88AA000"
+    $url_macos_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/1.5.7/v2raya-x86_64-macos.zip"
+    $sha_macos_x64 = "596F5CC2954B83936B525ABC0020709BF5545596914594ECB58C0D05DAC0A1D3"
+    $url_macos_arm64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/1.5.7/v2raya-aarch64-macos.zip"
+    $sha_macos_arm64 = "EA4A62E6178BF2333FCD45DDDE52B2F61431D097ABDA6076A5E5C66CF2427961"
     if OS.linux?
       url $url_linux_x64
       sha256 $sha_linux_x64
