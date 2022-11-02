2ab2c8faa06c79c0722baf9b6a5abb639f44cbb9
Markson <markson@MarksondeMac-mini.local>
2022-11-02 16:56:16 +0800

    Add v2raya-git

---
+++ Formula/v2raya-git.rb
@@ -0,0 +1,22 @@
+class V2raya-git < Formula
+    desc "Web-based GUI client of Project V"
+    homepage "https://v2raya.org"
+    license "AGPL-3.0-only"
+    version "20221004.r1225.11aa2b0"
+ 
+    url "https://github.com/v2rayA/v2rayA/archive/11aa2b0a982989983fd086510f8fea864ed92deb.zip"
+    sha256 "ac4384f7e12408f6734b38069fef6aa0e124c2050660e1570a08715e3b1b7a86"
+
+    depends_on "v2ray5"
+
+    def install
+      system "/bin/bash", "-c", "./build.sh"
+      bin.install "v2raya" => "v2raya-git"
+    end
+
+    service do
+      environment_variables V2RAYA_V2RAY_BIN: "#{HOMEBREW_PREFIX}/bin/v2ray5", V2RAYA_LOG_FILE: "/tmp/v2raya_feat_v5.log", V2RAYA_V2RAY_ASSETSDIR: "#{HOMEBREW_PREFIX}/share/v2ray5"
+      run [bin/"v2raya-git", "--lite"]
+      keep_alive true
+    end
+end
