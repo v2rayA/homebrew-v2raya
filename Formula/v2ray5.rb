dc660fbc895061d6c7645d7fdcf1ffd980f9160c
Markson <markson@2333ma.top>
2022-12-13 20:52:39 +0800

    v2ray5: update to version 5.1.0

--- Formula/v2ray5.rb
+++ Formula/v2ray5.rb
@@ -2,13 +2,13 @@ class V2ray5 < Formula
     desc "v2ray core from Project V"
     homepage "https://www.v2fly.org/"
     license "MIT License"
-    version "5.0.0"
+    version "5.1.0"
  
-    $url_linux_x64 = "https://github.com/v2fly/v2ray-core/releases/download/v5.0.0/v2ray-linux-64.zip"
+    $url_linux_x64 = "https://github.com/v2fly/v2ray-core/releases/download/v5.1.0/v2ray-linux-64.zip"
     $sha_linux_x64 = "e670334a1734b37361f5a1f12d79e989944b0f9556322f1ae710c2db16528e6a"
-    $url_macos_x64 = "https://github.com/v2fly/v2ray-core/releases/download/v5.0.0/v2ray-macos-64.zip"
+    $url_macos_x64 = "https://github.com/v2fly/v2ray-core/releases/download/v5.1.0/v2ray-macos-64.zip"
     $sha_macos_x64 = "fe649ebe10d507d0f04db3eeaccb6363cffac1fc42e517d0e10f3fc16c6ef5cd"
-    $url_macos_arm64 = "https://github.com/v2fly/v2ray-core/releases/download/v5.0.0/v2ray-macos-arm64-v8a.zip"
+    $url_macos_arm64 = "https://github.com/v2fly/v2ray-core/releases/download/v5.1.0/v2ray-macos-arm64-v8a.zip"
     $sha_macos_arm64 = "a916c2b1a041ad5771fa0153ceed24260e6b9e07e5f946caf80b949c699f44f4"
     if OS.linux?
       url $url_linux_x64
