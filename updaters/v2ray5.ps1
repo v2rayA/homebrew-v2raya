4aa536f445dbc1ada6b3369ec4ee419826c4e52f
Markson <markson@2333ma.top>
2023-01-16 22:50:12 +0800

    Fix hash check

--- updaters/v2ray5.ps1
+++ updaters/v2ray5.ps1
@@ -13,9 +13,9 @@ else {
     $Current_Linux64_Hash = Get-Content ./Formula/v2ray5.rb | Select-String 'sha_linux_x64 =' | ForEach-Object { ([string]$_).split('"')[1] }
     $Current_macOS_x64Hash = Get-Content ./Formula/v2ray5.rb | Select-String 'sha_macos_x64 =' | ForEach-Object { ([string]$_).split('"')[1] }
     $Current_macOS_arm64Hash = Get-Content ./Formula/v2ray5.rb | Select-String 'sha_macos_arm64 =' | ForEach-Object { ([string]$_).split('"')[1] }
-    $Latest_Linux64_Hash = curl -Ls "https://github.com/v2fly/v2ray-core/releases/download/v$Latest_Version/v2ray-linux-64.zip.dgst" | grep SHA256 | awk -F ' ' '{print $2}'
-    $Latest_macOS_x64Hash = curl -Ls "https://github.com/v2fly/v2ray-core/releases/download/v$Latest_Version/v2ray-macos-64.zip.dgst" | grep SHA256 | awk -F ' ' '{print $2}'
-    $Latest_macOS_arm64Hash = curl -Ls "https://github.com/v2fly/v2ray-core/releases/download/v$Latest_Version/v2ray-macos-arm64-v8a.zip.dgst" | grep SHA256 | awk -F ' ' '{print $2}'
+    $Latest_Linux64_Hash = curl -Ls "https://github.com/v2fly/v2ray-core/releases/download/v$Latest_Version/v2ray-linux-64.zip.dgst" | grep '256= ' | awk -F ' ' '{print $2}'
+    $Latest_macOS_x64Hash = curl -Ls "https://github.com/v2fly/v2ray-core/releases/download/v$Latest_Version/v2ray-macos-64.zip.dgst" | grep '256= ' | awk -F ' ' '{print $2}'
+    $Latest_macOS_arm64Hash = curl -Ls "https://github.com/v2fly/v2ray-core/releases/download/v$Latest_Version/v2ray-macos-arm64-v8a.zip.dgst" | grep S'256= ' | awk -F ' ' '{print $2}'
     (Get-Content -Path "./Formula/v2ray5.rb") -replace $Current_Linux64_Hash, $Latest_Linux64_Hash | Out-File "./Formula/v2ray5.rb"
     (Get-Content -Path "./Formula/v2ray5.rb") -replace $Current_macOS_x64Hash, $Latest_macOS_x64Hash | Out-File "./Formula/v2ray5.rb"
     (Get-Content -Path "./Formula/v2ray5.rb") -replace $Current_macOS_arm64Hash, $Latest_macOS_arm64Hash | Out-File "./Formula/v2ray5.rb"
