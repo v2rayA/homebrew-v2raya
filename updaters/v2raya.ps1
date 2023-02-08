5fbf3ded16e5e141818abbd18344f6239e2df96b
Markson Ho <markson@MarksondeMac-mini.local>
2023-02-08 12:55:38 +0800

    Rewrite build script

--- updaters/v2raya.ps1
+++ updaters/v2raya.ps1
@@ -9,9 +9,9 @@ if ([String]::IsNullOrEmpty($Version_Latest)) {
 If ($Version_Current -eq $Version_Latest) {
     Write-Output "Nothing to do, you have the latest version of v2raya."
 }else {
-    $New_SHA256_Darwin_A64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-aarch64-macos-sha256.txt"
-    $New_SHA256_Darwin_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-macos-sha256.txt"
-    $New_SHA256_Linux_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-linux-sha256.txt"
+    $New_SHA256_Darwin_A64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-aarch64-macos.sha256.txt"
+    $New_SHA256_Darwin_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-macos.sha256.txt"
+    $New_SHA256_Linux_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-linux.sha256.txt"
     $Old_SHA256_Darwin_A64 = Get-Content ./Formula/v2raya.rb | Select-String 'sha_macos_arm64 ='| ForEach-Object { ([string]$_).split('"')[1]} 
     $Old_SHA256_Darwin_x64 = Get-Content ./Formula/v2raya.rb | Select-String 'sha_macos_x64 =' | ForEach-Object { ([string]$_).split('"')[1]} 
     $Old_SHA256_Linux_x64 = Get-Content ./Formula/v2raya.rb | Select-String 'sha_linux_x64 =' | ForEach-Object { ([string]$_).split('"')[1]}
