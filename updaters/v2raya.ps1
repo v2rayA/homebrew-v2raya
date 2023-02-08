c35cd9a5e6132f4fcad4115e88b6af97793c4f8b
Markson Ho <markson@MarksondeMac-mini.local>
2023-02-08 13:03:01 +0800

    Fix typo

--- updaters/v2raya.ps1
+++ updaters/v2raya.ps1
@@ -9,9 +9,9 @@ if ([String]::IsNullOrEmpty($Version_Latest)) {
 If ($Version_Current -eq $Version_Latest) {
     Write-Output "Nothing to do, you have the latest version of v2raya."
 }else {
-    $New_SHA256_Darwin_A64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-aarch64-macos.sha256.txt"
-    $New_SHA256_Darwin_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-macos.sha256.txt"
-    $New_SHA256_Linux_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-linux.sha256.txt"
+    $New_SHA256_Darwin_A64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-aarch64-macos.zip.sha256.txt"
+    $New_SHA256_Darwin_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-macos.zip.sha256.txt"
+    $New_SHA256_Linux_x64 = curl -sL "https://github.com/v2rayA/homebrew-v2raya/releases/download/$Version_Latest/v2raya-x86_64-linux.zip.sha256.txt"
     $Old_SHA256_Darwin_A64 = Get-Content ./Formula/v2raya.rb | Select-String 'sha_macos_arm64 ='| ForEach-Object { ([string]$_).split('"')[1]} 
     $Old_SHA256_Darwin_x64 = Get-Content ./Formula/v2raya.rb | Select-String 'sha_macos_x64 =' | ForEach-Object { ([string]$_).split('"')[1]} 
     $Old_SHA256_Linux_x64 = Get-Content ./Formula/v2raya.rb | Select-String 'sha_linux_x64 =' | ForEach-Object { ([string]$_).split('"')[1]}
@@ -22,5 +22,5 @@ If ($Version_Current -eq $Version_Latest) {
     git commit "./Formula/v2raya.rb" -m "v2rayA: update to version $Version_Latest"
 }
 
-$version_Loyalsoldier_latest = ((Invoke-WebRequest "https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest" | ConvertFrom-Json ).tag_name)
-$version_Loyalsoldier_current = Get-Content "./Formula/v2raya.rb" | Select-String 'Loyalsoldier_version' | ForEach-Object { ([string]$_).split('"')[1]}
+# $version_Loyalsoldier_latest = ((Invoke-WebRequest "https://api.github.com/repos/Loyalsoldier/v2ray-rules-dat/releases/latest" | ConvertFrom-Json ).tag_name)
+# $version_Loyalsoldier_current = Get-Content "./Formula/v2raya.rb" | Select-String 'Loyalsoldier_version' | ForEach-Object { ([string]$_).split('"')[1]}
