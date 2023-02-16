94b1a9b6cd1f316034c294946a3c39aff6004500
markson <markson@archlinux>
2023-02-16 16:44:21 +0800

    Use Invoke-RestMethod

--- updaters/v2raya-git.ps1
+++ updaters/v2raya-git.ps1
@@ -1,4 +1,4 @@
-$Latest_Commit_ID = (((Invoke-WebRequest -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/feat_v5" | ConvertFrom-Json).commit).url).Split("/")[8]
+$Latest_Commit_ID = (((Invoke-RestMethod -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/feat_v5").commit).url).Split('/')[8]
 $Latest_Source_Url = "https://github.com/v2rayA/v2rayA/archive/$Latest_Commit_ID.zip"
 
 Invoke-WebRequest -Uri $Latest_Source_Url -OutFile "./v2rayA-$Latest_Commit_ID"
@@ -7,7 +7,7 @@ $Latest_File_Hash = (Get-FileHash "./v2rayA-$Latest_Commit_ID").hash
 $Current_Source_Url = Get-Content -Path "./Formula/v2raya-git.rb" | Select-String "url " | ForEach-Object { ([string]$_).split('"')[1]}
 $Current_File_Hash = Get-Content -Path "./Formula/v2raya-git.rb" | Select-String "sha256 " | ForEach-Object { ([string]$_).split('"')[1]}
 
-$Latest_Version_Date = ((((Invoke-WebRequest -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/feat_v5" | ConvertFrom-Json)."commit")."author") | ConvertTo-Json | ForEach-Object { ([string]$_).split('"')[11]} | ForEach-Object { ([string]$_).split('T')[0]}) -Replace "-",""
+$Latest_Version_Date = ((((((Invoke-RestMethod -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/feat_v5").commit).author) | ConvertTo-Json).Split('"')[11]).Split('T')[0]) -replace '-',''
 $Latest_Version = $Latest_Version_Date + "." + $(($Latest_Commit_ID)|cut -b 1-6)
 
 $Current_Version = Get-Content -Path "./Formula/v2raya-git.rb" | Select-String "version " | ForEach-Object { ([string]$_).split('"')[1]}
