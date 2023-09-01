660cad0fc7edb888750f528952b7ed9d67935e7c
Markson Hon <markson@2333ma.top>
2023-09-01 15:03:25 +0800

    v2raya: update to 2.2.2

--- Formula/v2raya.rb
+++ Formula/v2raya.rb
@@ -6,11 +6,11 @@ class V2raya < Formula
     
     $v2rayA_version = "2.2.2"
     $url_linux_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.2.2/v2raya-x86_64-linux.zip"
-    $sha_linux_x64 = "953d1c2c778a50f8711d4644bcaf5bb0180a8da78b8f5efe12.2.228bf63a224"
+    $sha_linux_x64 = "RealSha256_Linux_x64"
     $url_macos_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.2.2/v2raya-x86_64-macos.zip"
-    $sha_macos_x64 = "80c12680f9061bd978b6acfe65f5598468b6ece7ddac370f7a411997133ae9cf"
+    $sha_macos_x64 = "RealSha256_MacOS_x64"
     $url_macos_arm64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.2.2/v2raya-aarch64-macos.zip"
-    $sha_macos_arm64 = "81a697d44d3bcc44da07561804dbdbc75550e9d2c39191a2404d571e6c78aac5"
+    $sha_macos_arm64 = "RealSha256_MacOS_arm64"
 
     if OS.linux?
       url $url_linux_x64
