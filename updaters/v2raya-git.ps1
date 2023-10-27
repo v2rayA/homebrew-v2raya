a229e0e59de33ece4d5ee185c6ab240778740aff
Markson Hon <50002150+MarksonHon@users.noreply.github.com>
2023-10-27 21:27:28 +0800

    Update v2raya-git.ps1

--- updaters/v2raya-git.ps1
+++ updaters/v2raya-git.ps1
@@ -1,4 +1,4 @@
-$Latest_Commit_ID = (((Invoke-RestMethod -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/feat_v5").commit).url).Split('/')[8]
+$Latest_Commit_ID = (((Invoke-RestMethod -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/main").commit).url).Split('/')[8]
 $Latest_Source_Url = "https://github.com/v2rayA/v2rayA/archive/$Latest_Commit_ID.zip"
 
 Set-PSDebug -Strict -Trace 1
@@ -6,7 +6,7 @@ Set-PSDebug -Strict -Trace 1
 Invoke-WebRequest -Uri $Latest_Source_Url -OutFile "./v2rayA-$Latest_Commit_ID"
 $Latest_File_Hash = (Get-FileHash "./v2rayA-$Latest_Commit_ID").hash
 
-$Latest_Version_Date = ((((((Invoke-RestMethod -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/feat_v5").commit).author) | ConvertTo-Json).Split('"')[11]).Split('T')[0]) -replace '-',''
+$Latest_Version_Date = ((((((Invoke-RestMethod -Uri "https://api.github.com/repos/v2rayA/v2rayA/commits/main").commit).author) | ConvertTo-Json).Split('"')[11]).Split('T')[0]) -replace '-',''
 $Latest_Version = $Latest_Version_Date + "." + $(($Latest_Commit_ID)|cut -b 1-7)
 
 $Current_Version = Get-Content -Path "./Formula/v2raya-git.rb" | Select-String 'version ' | ForEach-Object { ([string]$_).split('"')[1]}
