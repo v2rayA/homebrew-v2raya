5d23ae3116224819f1bee762db5baeb3434ad2ad
Markson Pigeonzilla Plus <markson@archlinux.local>
2026-07-20 22:01:21 +0800

    v2raya: update to 2.4.8

--- Formula/v2raya.rb
+++ Formula/v2raya.rb
@@ -2,21 +2,28 @@ class V2raya < Formula
     desc "Web-based GUI client of Project V"
     homepage "https://v2raya.org"
     license "AGPL-3.0-only"
-    version "2.4.6"
-    
-    $v2rayA_version = "2.4.6"
-    $url_linux_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.6/v2raya-x86_64-linux.zip"
-    $sha_linux_x64 = "16b8f3cdb3710707523946011663a2ca7585e4535fc6adb51f905d6a957067ae"
-    $url_macos_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.6/v2raya-x86_64-macos.zip"
-    $sha_macos_x64 = "a3008fd94c3675d1acadaec390fb4abf78604f71e8b5524217b30e6b7f4c2c51"
-    $url_macos_arm64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.6/v2raya-aarch64-macos.zip"
-    $sha_macos_arm64 = "b7ced1407dd73b8745836ca30052d9369a834cdc8e10c4b985b63c1e16beacc2"
+    version "2.4.8"
+
+    $v2rayA_version = "2.4.8"
+    $url_linux_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.8/v2raya-x86_64-linux.zip"
+    $sha_linux_x64 = "c06738fc2a9ed33f9211255af32258ebc1908d66e97c976138759a8aa22a5339"
+    $url_linux_arm64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.8/v2raya-arm64-linux.zip"
+    $sha_linux_arm64 = "272f3fc153b6134007e952258ca63f99e46ba0076b23b3355011b5a8f4582327"
+    $url_macos_x64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.8/v2raya-x86_64-macos.zip"
+    $sha_macos_x64 = "f8e1b30de5d0eb124f61b9385454eb48da3cf41cb527b2f3290072aeac27d36e"
+    $url_macos_arm64 = "https://github.com/v2rayA/homebrew-v2raya/releases/download/2.4.8/v2raya-aarch64-macos.zip"
+    $sha_macos_arm64 = "a6d76b7a80a290c72f518003138f08c0fbc45c65f07dbfb0e5d6d8d5fc839532"
 
     depends_on "v2ray-rules-dat"
 
     if OS.linux?
-      url $url_linux_x64
-      sha256 $sha_linux_x64
+      if Hardware::CPU.arm?
+        url $url_linux_arm64
+        sha256 $sha_linux_arm64
+      else
+        url $url_linux_x64
+        sha256 $sha_linux_x64
+      end
     elsif Hardware::CPU.intel?
       url $url_macos_x64
       sha256 $sha_macos_x64
